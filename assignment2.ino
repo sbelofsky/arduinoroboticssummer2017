a9c06904dccfa33cef1183f3ae21f22da37b920d
sbelofsky <sabine.belofsky@cca.edu>
2017-05-25 20:07:51 -0700

    Update assignment2.ino

--- assignment2.ino
+++ assignment2.ino
@@ -1,3 +1,4 @@
+//AnalogInput assignment 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -23,4 +24,44 @@ void loop() {
   digitalWrite(ledPin, LOW);
   // stop the program for for <sensorValue> milliseconds:
   delay(sensorValue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Turning on the LED based on brightness
+const int analogPin = A0;    // pin that the sensor is attached to
+const int ledPin = 13;       // pin that the LED is attached to
+const int threshold = 400;   // an arbitrary threshold level that's in the range of the analog input
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize serial communications:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value of the potentiometer:
+  int analogValue = analogRead(analogPin);
+
+ 
+  if (analogPin < 800) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  // print the analog value:
+  Serial.println(analogValue);
+  delay(1);        // delay in between reads for stability
 }
