2ea92535a98e693f60979361ffeaa10ad4584496
sbelofsky <sabine.belofsky@cca.edu>
2017-05-26 13:32:52 -0700

    Update assignment2.ino

--- assignment2.ino
+++ assignment2.ino
@@ -28,12 +28,7 @@ void loop() {
 
 
 
-
-
-
-
-
-//Turning on the LED based on brightness
+//Turning on the LED when its bright
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -54,6 +49,32 @@ void loop() {
   
   digitalWrite(ledPin, LOW);
   if (sensorValue <= 620)
+
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
+}
+
+//turning on the LED when its dark
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+ Serial.begin (9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+   Serial.println (sensorValue); 
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  if (sensorValue <= 840)
+  
+  digitalWrite(ledPin, LOW);
+  if (sensorValue >= 920)
 
   // stop the program for for <sensorValue> milliseconds:
   delay(sensorValue);
