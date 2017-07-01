cd26526c5a469c31f7130dfe59e9637556088582
sbelofsky <sabine.belofsky@cca.edu>
2017-07-01 01:16:36 -0700

    Update Robot_PWM_stand.ino

--- Final/coding/Robot_PWM_stand.ino
+++ Final/coding/Robot_PWM_stand.ino
@@ -1,20 +1,4 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
